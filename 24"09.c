56de2a6f0a5f25231f316574dd8392bd16367415
haraldboos <haraldboos16@gmail.com>
2024-09-24 01:47:55 +0530

    stuck in stuut

---
+++ 24"09.c
@@ -0,0 +1,72 @@
+
+// integer float to charater
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main()
+// {
+//     char fuck[10]; 
+//     float miya;
+//     printf("enter th sex ssss ahah:-: %c\n",fuck);
+//     scanf("%f",&miya);
+//     sprintf(fuck,"%f",miya);
+//     printf("enter th sex ssss ahah:-: %s\n",fuck);
+// }
+
+// charater to integer
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main(){
+//     char inp[10];
+//     int x;
+//     printf("Enter The Number:\n");
+//     scanf("%s",&inp);
+//     x = atoi(inp);
+//     printf("convert %s in to numeric number %d",inp+23,x+23);
+// }
+
+
+
+// strature
+#include <stdio.h>
+#include <stdlib.h>
+// struct oopai
+// {
+//     int miya,sunny;
+// };
+
+// int main(){
+//     struct oopai ool1,ool2,ool3;
+//     ool1.miya=10;
+//     ool1.sunny=20;
+//     ool2.miya=30;
+//     ool2.sunny=40;
+//     ool3.miya=ool1.miya^ool2.miya;
+//     ool3.sunny = ool1.sunny^ool2.sunny;
+//     printf("oopai ools (miya,sunny) %d,%d",ool3.miya,ool3.sunny);
+// }
+
+
+// struter arrsay
+struct oooma
+{
+int x,g;
+
+};
+int main(){
+    struct oooma o1[3];
+    int i;
+     for ( i = 0; i < 2; i++)
+     {
+printf("Enter x & y values:");
+scanf("%d%d",&o1[i].x,&o1[i].g);
+
+     }
+     printf("\nEnter values done now fuck\n");
+       for (i = 0; i < 2; i++) {
+        printf("o1[%d] -> x: %d, g: %d\n", i, o1[i].x, o1[i].g);
+    }
+     
+    
+    
+}
