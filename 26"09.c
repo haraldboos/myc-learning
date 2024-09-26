622b6cb54f275a710b8cff0112a8c45839a5e079
haraldboos <haraldboos16@gmail.com>
2024-09-26 23:38:15 +0530

    union

---
+++ 26"09.c
@@ -0,0 +1,21 @@
+// union
+#include<stdio.h>
+struct ool1
+{
+    int f;
+    char m[50];
+};
+union fk1
+{
+int y;
+char s3[50];
+};
+
+int main(){
+    struct ool1 ool2;
+    union fk1 fk2;
+    printf("size of ool2 (st): %ld bytes\n",sizeof(ool2));
+    printf("size of fk1 (st): %ld bytes\n",sizeof(fk2));
+
+    
+}
