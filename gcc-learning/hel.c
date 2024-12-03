bc6ba36705154ab890cb00617f1d66c91c326fa1
Harald Branaven.jr <100700198+haraldboos@users.noreply.github.com>
2024-12-03 23:16:32 +0530

    Update hel.c

--- gcc-learning/hel.c
+++ gcc-learning/hel.c
@@ -4,7 +4,7 @@ int main() {
     int count = 0; // Initialize a counter
 
     while (count < 5) { // Loop 5 times
-        printf("Hello, World!\n"); // Print "Hello, World!"
+        printf("Hello, World! \n"); // Print "Hello, World!"
         count++; // Increment the counter
     }
     printf("conplete task");
