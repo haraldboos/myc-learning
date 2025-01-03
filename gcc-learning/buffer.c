6c8acac0665138947dd414e1232f821fe1b693c3
haraldboos <haraldboos16@gmail.com>
2025-01-03 23:19:43 +0530

    buffer

---
+++ gcc-learning/buffer.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+int main(){
+    char f[3],u[3],c[3],k[3];
+    printf ("1 f [%s] u [%s] c [%s] k [%s]\n",f,u,c,k);
+
+    printf("enter the value of f\n");
+    scanf("%s",f);
+    printf ("2 f [%s] u [%s] c[%s] k[%s]\n",f,u,c,k);
+
+    printf("enter the value of u\n");
+    scanf("%s",u);
+    printf ("3 f [%s] u [%s] c[%s] k[%s]\n",f,u,c,k);
+
+    printf("enter the value of c\n");
+    scanf("%s",c);
+    printf ("4 f [%s] u [%s] c[%s] k[%s]\n",f,u,c,k);
+
+    printf("enter the value of k\n");
+    scanf("%s",k);
+
+printf ("5 f [%s] u [%s] c[%s] k[%s]\n",f,u,c,k);
+}
