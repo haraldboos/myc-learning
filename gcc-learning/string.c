68105407cf167b4ee984656c62f7430cad7dd9d2
haraldboos <haraldboos16@gmail.com>
2025-01-26 23:18:53 +0530

    gcc 3 done

--- gcc-learning/string.c
+++ gcc-learning/string.c
@@ -2,17 +2,21 @@
 int main()
 {
     /* code */
+    int j = 0;
     char a[6]="miakl";
-    char l[5]="oolandi";
+    char l[5]="landi";
     // scanf("%s",&l);
 
     char b[6]="wor\0de";
+    scanf("%s",l);
     // char *c=&b[4];
     // char a[5]="fuck";
     // a[3]='\0';
     printf("%s @a %p\n",a,a);
     printf("%s @b %p\n",b,b);
     printf("%s @l %p\n",l,l);
+    printf("%d @j %p\n",j,&j);
+
         // printf("%s @ %p\n",c,c);
 
 printf("memory of a ******************\n");
@@ -26,6 +30,10 @@ for (int i = 0; i < 6; i++)
 printf("memory of l ******************\n");
 for (int i = 0; i < 5; i++)
     printf("%c @ %p \n",l[i],&l[i]);
-
-    return 0;
+if(j){
+    printf("access Granded");
+}else{
+    printf("access denied");
+}
+return 0;
 }
