41d2c7d7dca019f0bc075ed76da21be948f31d73
haraldboos <haraldboos16@gmail.com>
2025-01-31 23:54:58 +0530

    task

--- gcc-learning/hel.c
+++ gcc-learning/hel.c
@@ -3,6 +3,7 @@
 int main() { 
     int count = 0; // Initialize a counter
 
+
     while (count < 5) { // Loop 5 times
         printf("Hello, World! \n"); // Print "Hello, World!"
         count++; // Increment the counter
